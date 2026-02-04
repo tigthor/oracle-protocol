2d6b31e2925cc2c39ded41290581f7c7c67f3500
tigthor <tigthor@users.noreply.github.com>
2026-02-04 11:00:00 +0000

    feat(api): add ScheduleModule for cron job support

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
 import { MarketsModule } from "./modules/markets/markets.module";
 
 @Module({
-  imports: [MarketsModule],
+  imports: [ScheduleModule.forRoot(), MarketsModule],
 })
 export class AppModule {}
