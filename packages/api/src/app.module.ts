cf271e12668adfb19b673f23b525543c5feb9e0c
tigthor <tigthor@users.noreply.github.com>
2026-02-04 14:00:00 +0000

    feat(api): wire WebSocket gateway and resolution service into app

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 import { MarketsModule } from "./modules/markets/markets.module";
+import { WebSocketModule } from "./modules/websocket/websocket.module";
+import { ResolutionModule } from "./modules/resolution/resolution.module";
 
 @Module({
-  imports: [ScheduleModule.forRoot(), MarketsModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    MarketsModule,
+    WebSocketModule,
+    ResolutionModule,
+  ],
 })
 export class AppModule {}
